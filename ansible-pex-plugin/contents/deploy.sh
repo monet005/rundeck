0411b8a9a0c9a68286c72d456e7992b1cc9bfa15
monet005 <monet.velasco@gmail.com>
2021-08-26 08:54:15 +0800

    updated plugin

--- ansible-pex-plugin/contents/deploy.sh
+++ ansible-pex-plugin/contents/deploy.sh
@@ -3,19 +3,71 @@
 set -o pipefail
 
 PEX_BINARY=$(basename "$RD_CONFIG_PEX_SOURCE_URL")
-TMPDIR=$RD_PLUGIN_TMPDIR/pextmp
-PEX_CACHE=$RD_CONFIG_WORKDIR/.pex
+WORKDIR=$(mktemp -d "$RD_PLUGIN_TMPDIR/pextmp.XXXXX")
+SSH_KEY_STORAGE_PATH=$(mktemp "$WORKDIR/ssh-keyfile.XXXXX")
+EXTRA_VARS_PATH=$(mktemp "$WORKDIR/extra-vars.XXXXX")
+PEX_CACHE=$WORKDIR/.pex
+ROLESDIR=${WORKDIR}/$(dirname "$RD_CONFIG_PLAYBOOK")/roles
 extra_args=()
+trap 'rm -rf $WORKDIR' EXIT
 
-cd "$RD_CONFIG_WORKDIR" || exit
+
+function ansible-galaxy () {
+    if [[ -n "$RD_CONFIG_ANSIBLE_GALAXY_SOURCE" ]]; then
+        echo "Downloading the ansible source code from ansible galaxy: $RD_CONFIG_ANSIBLE_GALAXY_SOURCE"
+        if ! PEX_ROOT=${PEX_CACHE} PEX_SCRIPT=ansible-galaxy ./"$PEX_BINARY" \
+            install \
+            --role-file "$RD_CONFIG_ANSIBLE_GALAXY_SOURCE" \
+            --roles-path "$ROLESDIR" \
+            --ignore-certs
+        then
+            echo "Unable to download the ansible galaxy roles"; exit 1
+        fi
+    else
+        echo "ansible-galaxy was selected but no source url provided."; exit 1
+    fi
+}
+
+function tarfile () {
+    if [[ -n "$RD_CONFIG_TARFILE_SOURCE" ]]; then
+        echo "Downloading the ansible source code from a tar archive: $RD_CONFIG_TARFILE_SOURCE"
+        if ! tar xvf "$(basename "$RD_CONFIG_TARFILE_SOURCE")"
+        then
+            echo "Unable to extract the tarfile"; exit 1
+        fi
+    else
+        echo "tarfile was selected but no source url provided."; exit 1
+    fi
+}
+
+function git-repo () {
+    if [[ -n "$RD_CONFIG_GITREPO_SOURCE" ]]; then
+        echo "Downloading the ansible source code from git repo: $RD_CONFIG_GITREPO_SOURCE"
+        if ! git clone "$RD_CONFIG_GITREPO_SOURCE" > /dev/null 2>&1
+        then
+            echo "Failed to download from $RD_CONFIG_GITREPO_SOURCE repo"; exit 1
+        fi
+    else
+        echo "git-repo was selected but no source url provided."; exit 1
+    fi
+}
+
+cd "$WORKDIR" || exit
+
 curl -fLOsS "$RD_CONFIG_PEX_SOURCE_URL" && echo "Ansible PEX downloaded successfully"
 chmod +x "$PEX_BINARY"
 echo "Running with below ansible version and settings:"
 PEX_ROOT=${PEX_CACHE} PEX_SCRIPT=ansible ./"$PEX_BINARY" --version
 
-mkdir -p "$TMPDIR"
-SSH_KEY_STORAGE_PATH=$(mktemp "$TMPDIR/ssh-keyfile.XXXXX")
-EXTRA_VARS_PATH=$(mktemp "$TMPDIR/extra-vars.XXXXX")
+if [[ -n "$RD_CONFIG_SOURCE_CODE_DOWNLOAD_OPTIONS" ]]; then
+    IFS=',' read -r -a download_options <<< "$RD_CONFIG_SOURCE_CODE_DOWNLOAD_OPTIONS"
+    for download_option in "${download_options[@]}"; do
+        $download_option
+    done
+else
+    echo "No download source provided"; exit 1
+fi
+
 echo "$RD_CONFIG_SSH_KEY_STORAGE_PATH" > "$SSH_KEY_STORAGE_PATH"
 echo "$RD_CONFIG_EXTRA_VARS" | tr ',' '\n' > "$EXTRA_VARS_PATH"
 sed -i 's/^ *//' "$EXTRA_VARS_PATH"
@@ -38,9 +90,13 @@ if [[ $RD_CONFIG_BECOME == true ]]; then
     extra_args+=(--become)
 fi
 
-trap 'rm -rf $TMPDIR' EXIT
+if [[ $RD_CONFIG_CHECK_MODE == true ]]; then
+    extra_args+=(--check)
+fi
 
-echo "${extra_args[@]}"
+if [[ -n "$RD_CONFIG_LIMIT" ]]; then
+    extra_args+=(--limit="$RD_CONFIG_LIMIT")
+fi
 
 if ! PEX_ROOT=${PEX_CACHE} PEX_SCRIPT=ansible-playbook ./"$PEX_BINARY" \
     "$RD_CONFIG_PLAYBOOK" \
